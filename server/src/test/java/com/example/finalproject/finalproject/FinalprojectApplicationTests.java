a73b5986367c29e862d5e2d777e938095423091f
Mitchell McShane <mitchell.mcshane1987@gmail.com>
2021-02-17 12:22:46 +0000

    back-end tests working

--- server/src/test/java/com/example/finalproject/finalproject/FinalprojectApplicationTests.java
+++ server/src/test/java/com/example/finalproject/finalproject/FinalprojectApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.finalproject.finalproject;
 
+import com.example.finalproject.finalproject.models.Freelancer;
+import com.example.finalproject.finalproject.models.Job;
+import com.example.finalproject.finalproject.repositories.FreelancerRepository;
+import com.example.finalproject.finalproject.repositories.JobRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FinalprojectApplicationTests {
 
+	@Autowired
+	FreelancerRepository freelancerRepository;
+
+	@Autowired
+	JobRepository jobRepository;
+
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	public void createAndSaveFreelancerAndJobs(){
+
+		Freelancer bob = new Freelancer("Bob","bob123@fakemail.com", "Robert123");
+		freelancerRepository.save(bob);
+
+		Job web = new Job("Front-End developer", "create an app for freelancers to search for work", 500, true,
+				"JS, HTML, CSS", bob);
+		jobRepository.save(web);
+	}
+
 
 }
