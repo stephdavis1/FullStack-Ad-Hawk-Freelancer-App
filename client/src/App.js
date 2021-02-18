46b2c7f4fe5324698486b37bee828a6938bd8e27
Steph Davis <stephdavisdesign@hotmail.com>
2021-02-18 13:18:31 +0000

    debugging

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,9 @@ import AppContainer from "./containers/AppContainer";
 function App() {
   return (
     <>
-      <h1>Hello this will be the front page</h1>
+      <h1>Hire the best freelancers for any job, online.</h1>
+      
+      <h2>Millions of people use Ad-Hawk to turn their ideas into reality.</h2>
 
       <AppContainer />
     </>
