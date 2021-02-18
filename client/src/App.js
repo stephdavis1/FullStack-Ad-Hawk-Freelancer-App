7490ea62c90624c4e835624b36a38b00db3773d8
Mitchell McShane <mitchell.mcshane1987@gmail.com>
2021-02-18 17:03:49 +0000

    fixing search functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,13 @@
 import './App.css';
 import AppContainer from "./containers/AppContainer";
 import HomePage from './components/homepage/HomePage';
-import Header from './components/headers/Header';
-import Search from './components/search/Search';
 
 function App() {
   return (
     <>
       <HomePage />
-      <Header />
-      <Search />
 
-      {/* <AppContainer /> */}
+      <AppContainer />
     </>
   );
 }
