36f8906346b7f243b28d12e22c7f89de4f20bca7
Mitchell McShane <mitchell.mcshane1987@gmail.com>
2021-02-20 15:39:28 +0000

    able to fetch from backend api

--- server/src/main/java/com/example/finalproject/finalproject/repositories/JobRepository.java
+++ server/src/main/java/com/example/finalproject/finalproject/repositories/JobRepository.java
@@ -1,6 +1,5 @@
 package com.example.finalproject.finalproject.repositories;
 
-import com.example.finalproject.finalproject.models.Freelancer;
 import com.example.finalproject.finalproject.models.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,11 +9,11 @@ import java.util.List;
 @Repository
 public interface JobRepository extends JpaRepository<Job, Long> {
 
-    // List jobs that are accepted as True
-    List<Job> findByAcceptedTrue();
-
-    // List jobs that are accepted as False
-    List<Job> findByAcceptedFalse();
+//    // List jobs that are accepted as True
+//    List<Job> findByAcceptedTrue();
+//
+//    // List jobs that are accepted as False
+//    List<Job> findByAcceptedFalse();
 
 
 }
