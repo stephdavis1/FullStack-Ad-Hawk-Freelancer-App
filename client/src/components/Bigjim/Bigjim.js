e95eb00133448f7604e60a50d4cb6bb540247d24
Mitchell McShane <mitchell.mcshane1987@gmail.com>
2021-02-20 20:56:25 +0000

    fixed NavBar to look for jobs

---
+++ client/src/components/Bigjim/Bigjim.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Jumbotron, Container } from 'react-bootstrap';
+import './Bigjim.css';
+
+const Bigjim = () => {
+    return (
+        <div>
+            <Jumbotron fluid className="jumbotron">
+                <h1>Look for Jobs</h1>
+            </Jumbotron>
+        </div>
+    )
+
+}
