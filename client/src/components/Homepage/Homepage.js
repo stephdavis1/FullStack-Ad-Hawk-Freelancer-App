12a7da51b288ad449ef397ef9d9dca599f1a2251
Mitchell McShane <72915815+Mitchell-McShane@users.noreply.github.com>
2021-02-25 18:51:32 +0000

    Update Homepage.js

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import Button from "react-bootstrap/Button";
 
 const Homepage = () => {
 
     return (
-        <div>
-            <h1>Hire the best freelancers for any job, online.</h1>
-            <h2>Millions of people use Ad-hawk to turn their ideas into reality</h2>
-            <button>Start Now</button>
+        <div class="container bg-overlay">
+            <div class="homepage-logo">
+                <img src='https://i.imgur.com/5gwyncf.png' width="280" height="100" alt="ad-hawk"></img>
+            </div>
+            <br />
+
+            <div class="homepage-h1">
+                <h1>Hire the best freelancers for any job, online.</h1>
+                <br />
+            </div>
+            <div class="homepage-h4">
+                <h4>Millions of people use Ad-hawk to turn their ideas into reality</h4>
+            </div>
+            <br />
+            <div class="homepage-button">
+                <Button href="/alljobs">Get Started</Button>
+            </div>
         </div>
     )
 };
