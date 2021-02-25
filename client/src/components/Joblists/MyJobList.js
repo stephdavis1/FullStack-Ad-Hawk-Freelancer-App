178121ec2e8d6723e4d6994cffb28077360bc3b8
Steph Davis <stephdavisdesign@hotmail.com>
2021-02-25 16:29:01 +0000

    my jobs list updated

---
+++ client/src/components/Joblists/MyJobList.js
@@ -0,0 +1,20 @@
+import Job from './Job';
+
+const MyJobList = ({ jobs, updateJob, deleteJob }) => {
+    const jobNodes = jobs.map(job => {
+        return <Job
+            key={job._id}
+            job={job}
+            updateJob={updateJob}
+            deleteJob={deleteJob} />
+    });
+    if (!jobs) {
+        return null
+    }
+    return (
+        <>
+            {jobNodes}
+        </>
+    )
+}
+export default MyJobList;
